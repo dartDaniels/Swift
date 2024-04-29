356186af62b0b071df8f364534b08a2a98e68b10
dartDaniels <152316088+dartDaniels@users.noreply.github.com>
2024-04-29 18:58:42 +0600

    Add files via upload

---
+++ LabTask/LabTask/Source/LabTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabTaskApp.swift
+//  LabTask
+//
+//  Created by Данила Казмирук on 21.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LabTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
