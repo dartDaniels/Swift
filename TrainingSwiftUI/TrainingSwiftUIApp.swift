22e6b365691a07c57fd2aab8fe9b926c0c99e4b4
danilakazmiruk <danilakazmiruk@mail.ru>
2024-06-25 23:52:08 +0600

    Initial Commit

---
+++ TrainingSwiftUI/TrainingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingSwiftUIApp.swift
+//  TrainingSwiftUI
+//
+//  Created by Данила Казмирук on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrainingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
